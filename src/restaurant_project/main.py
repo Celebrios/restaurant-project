b98674dbd35ccf18027be5126e3008d1dcd4ded7
celebrios <dolgonadr16@gmail.com>
2026-03-11 21:44:39 +0700

    added starting libs

--- src/restaurant_project/main.py
+++ src/restaurant_project/main.py
@@ -1,7 +1,3 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, Field, EmailStr, field_validator, model_validator
-from typing import Optional, List
-from datetime import datetime
-from enum import Enum
 
 app = FastAPI()
---
+++ src/restaurant_project/schemas.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel, Field, EmailStr, field_validator, model_validator
+from typing import Optional, List
+from datetime import datetime
+from enum import Enum
